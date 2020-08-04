2456f7b09efa422a333ed0d413c56a19390313cf
Carla Widmer <carla.widmer@wuerth-itensis.com>
2020-08-04 15:14:27 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+openMenu8 = () => {
+  location.href = 'mema8a.html';
+  console.log('redirected to mema8a');
+};
+openMenu6 = () => {
+    location.href = 'mema6a.html';
+    console.log('redirected to mema6a');
+};
+
+absenzErfassen = () => {
+  alert('button clicked')
+};
